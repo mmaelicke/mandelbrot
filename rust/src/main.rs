0b3b6a98d478c79744a803df29954ffdfb107f41
Mirko Mälicke <mirko@maelicke-online.de>
2018-11-20 05:38:47 +0100

    added Rust pixel conversion

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,6 +2,37 @@ extern crate num;
 use num::Complex;
 use std::str::FromStr;
 
+#[allow(dead_code)]
+
+/// Convert image pixel to Complex number
+///
+/// bounds specifies the size of the image in pixel and pixel is the pixel
+/// that shall be converted to a Complex number. upper_left and lower_right
+/// are the bounding points of the Complex plane, where the image should be
+/// mapped to.
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64> {
+    // get width and height of the image
+    let (width, height) = (upper_left.re - lower_right.re,
+                           upper_left.im - lower_right.im);
+
+    // return the Complex number.
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+                              Complex {re: -1.0, im: 1.0},
+                              Complex {re: 1.0, im: -1.0}),
+               Complex {re: -0.5, im: -0.5})
+}
 
 /// Convert coordinate pair to Complex
 ///
